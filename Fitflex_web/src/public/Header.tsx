a2d41bb040ee3ac21c92e654357328ca5df418f0
Aashrit <aashrit17038@gmail.com>
2025-07-29 11:33:28 +0545

    header add

---
+++ Fitflex_web/src/public/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <div className="text-center mb-6">
+        <h1 className="text-4xl font-bold text-purple-400">🔥 Fitness Dashboard</h1>
+        <p className="text-gray-400 mt-1">Track your calories and exercise easily!</p>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
